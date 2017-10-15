044b4a842604e0427c48a76a023c67c6f669237d
sjsu2016 <Justin@Justins-MacBook-Pro.local>
2017-10-14 21:54:08 -0700

    content-script

--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -1,8 +1,19 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		console.debug('content-script');
-		$('body').html('extension button clicked');
-		alert('content-script ' + $('body').html());
+		$('body').html('Content script making Ajax request...');
+		var jqxhr = $.ajax( "http://localhost:9099?text=bar" )
+					  .done(function(data) {
+					    console.log("success " + data.foo );
+					    $('body').append('<b>success</b>:' + data.foo);
+					  })
+					  .fail(function() {
+					    alert( "error" );
+					  })
+					  .always(function() {
+					    console.log( "complete" );
+					  });
+		//alert('content-script ' + $('body').html());
 	}
 );
 document.body.addEventListener('click', function(arg1, arg2) {
