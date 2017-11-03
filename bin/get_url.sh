6083810782ba1845440941ccda2afa59d6462fb5
sjsu2016 <Justin@Justins-MacBook-Pro.local>
2017-11-02 21:59:37 -0700

    Titles - works for up to 6 urls

--- bin/get_url.sh
+++ bin/get_url.sh
@@ -1,3 +1,3 @@
 echo "$1"
 
-docker run --rm --cap-add=SYS_ADMIN --entrypoint=/usr/bin/google-chrome-unstable yukinying/chrome-headless-browser --headless --disable-gpu --dump-dom "$1"
+docker run --stop-timeout 3 --rm --cap-add=SYS_ADMIN --entrypoint=/usr/bin/google-chrome-unstable yukinying/chrome-headless-browser --headless --disable-gpu --dump-dom "$1"
