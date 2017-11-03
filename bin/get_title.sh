ee0d2d380809148a473ff7e4e596748850d222bf
sjsu2016 <Justin@Justins-MacBook-Pro.local>
2017-11-02 21:53:14 -0700

    Titles working when just 3 of them. Spews out a lot of error output when more.

--- bin/get_title.sh
+++ bin/get_title.sh
@@ -1,1 +1,3 @@
-cat - | grep "<title" | perl -pe 's{.*title>(.*?)</title.*}{$1}g'
+echo "$1" | grep --line-buffered "<title" | tee -a insideGetTitle.tmp | perl -pe 's{.*(title.*?>.*?</title>).*}{$1}g'
+
+#cat -  | tee body.tmp | grep --line-buffered "<title" | tee titlewithtag.tmp | gstdbuf -i0 -o0 -e0 perl -pe 's{.*title.*?>(.*?)</title.*}{$1}g' | tee afterPerlRegex.tmp
